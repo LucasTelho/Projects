5f998db432aa16661341ce56e762cc79e5cd7483
Lucas Telho <lucas.santos.telho@gmail.com>
2023-09-01 16:21:01 -0300

    Update 01/09
    Remoção de itens desnecessários na pasta.

--- Custom-Dropdown/script.js
+++
@@ -1,8 +0,0 @@
-let dropdown = document.querySelector('.dropdown');
-dropdown.onclick = function (){
-    dropdown.classList.toggle('active');
-}
-
-function show(a){
-    document.querySelector('.text02').value = a;
-}
